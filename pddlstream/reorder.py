cc4762a4ccd863fe72a46db5175d08d77d05eb07
Caelan Garrett <caelan@mit.edu>
2018-05-08 14:56:51 -0400

    Added some random code

--- pddlstream/reorder.py
+++ pddlstream/reorder.py
@@ -5,6 +5,23 @@ from heapq import heappush, heappop
 from pddlstream.utils import INF
 from pddlstream.visualization import get_partial_orders
 
+def compute_expected_cost(stream_plan):
+    if stream_plan is None:
+        return INF
+    expected_cost = 0
+    for result in reversed(stream_plan):
+        info = result.instance.external.info
+        expected_cost = info.overhead + info.p_success * expected_cost
+    return expected_cost
+
+def get_stream_dag(stream_plan):
+    orders = get_partial_orders(stream_plan)
+    incoming_edges = defaultdict(set)
+    outgoing_edges = defaultdict(set)
+    for v1, v2 in orders:
+        incoming_edges[v2].add(v1)
+        outgoing_edges[v1].add(v2)
+    return incoming_edges, outgoing_edges
 
 def cmp(v1, v2):
     i1 = v1.instance.external.info
@@ -23,16 +40,7 @@ def cmp(v1, v2):
     # TODO: actions are extremely unlikely to work
     return 0
 
-
-def get_stream_dag(stream_plan):
-    orders = get_partial_orders(stream_plan)
-    incoming_edges = defaultdict(set)
-    outgoing_edges = defaultdict(set)
-    for v1, v2 in orders:
-        incoming_edges[v2].add(v1)
-        outgoing_edges[v1].add(v2)
-    return incoming_edges, outgoing_edges
-
+##################################################
 
 def forward_topological_sort(stream_plan):
     if stream_plan is None:
@@ -54,6 +62,7 @@ def forward_topological_sort(stream_plan):
                 heappush(queue, (key(v2), v2))
     return ordering
 
+##################################################
 
 def backward_topological_sort(stream_plan):
     # Works well because DP reduces to this when no choices
@@ -80,19 +89,9 @@ def backward_topological_sort(stream_plan):
                 heappush(queue, (key, v2))
     return list(reversed(reversed_ordering))
 
-
-def compute_expected_cost(stream_plan):
-    if stream_plan is None:
-        return INF
-    expected_cost = 0
-    for result in reversed(stream_plan):
-        info = result.instance.external.info
-        expected_cost = info.overhead + info.p_success * expected_cost
-    return expected_cost
-
+##################################################
 
 Subproblem = namedtuple('Subproblem', ['cost', 'head', 'subset'])
-
 
 def dynamic_programming(stream_plan):
     # 2^N rather than N!
@@ -115,6 +114,7 @@ def dynamic_programming(stream_plan):
             layers.append(layer)
             layer = [v]
     layers.append(layer)
+    # TODO: not really total order. Intead do partial order, top sort, and check if dominated
 
     subset = frozenset()
     queue = deque([subset]) # Acyclic because subsets
@@ -147,3 +147,39 @@ def dynamic_programming(stream_plan):
         ordering.append(subproblem.head)
         subset = subproblem.subset
     return ordering
+
+##################################################
+
+def partial_ordered(plan):
+    # https://www.aaai.org/ocs/index.php/ICAPS/ICAPS10/paper/viewFile/1420/1539
+    # http://repository.cmu.edu/cgi/viewcontent.cgi?article=1349&context=compsci
+    # https://arxiv.org/pdf/1105.5441.pdf
+    # https://pdfs.semanticscholar.org/e057/e330249f447c2f065cf50db9dfaddad16aaa.pdf
+    # https://github.mit.edu/caelan/PAL/blob/master/src/search/post_processing.cc
+
+    instances = instantiate_plan(plan)
+    orders = set()
+    primary_effects = set() # TODO: start and goal operators here?
+    for i in reversed(xrange(len(instances))):
+        for pre in instances[i].preconditions:
+            for j in reversed(xrange(i)):
+                #if pre in instances[j].effects:
+                if any(eff == pre for eff in instances[j].effects):
+                    orders.add((j, i))
+                    primary_effects.add((j, pre))
+                    break
+        for eff in instances[i].effects:
+            for j in xrange(i):
+                if any((pre.head == eff.head) and (pre.value != eff.value) for pre in instances[j].preconditions):
+                    orders.add((j, i))
+            if (i, eff) in primary_effects:
+                for j in xrange(i):
+                    if any((eff2.head == eff.head) and (eff2.value != eff.value) for eff2 in instances[j].effects):
+                        orders.add((j, i))
+    # TODO: could remove transitive
+    # TODO: this isn't so helpful because it will choose arbitrary streams until an action is feasible (i.e. not intelligent ones)
+    for i, (action, args) in enumerate(plan):
+        print i, action, args #, instances[i].preconditions, instances[i].effects
+    print orders
+    print primary_effects
+    print topological_sort(range(len(plan)), orders, lambda v: hasattr(plan[v][0], 'stream'))
