7ec5ee07f8b00dff93a61936dc77499d58132c69
Caelan Garrett <caelan@mit.edu>
2019-01-30 23:40:40 -0500

    Cleaning up constraint solver

--- examples/continuous_tamp/constraint_solver.py
+++ examples/continuous_tamp/constraint_solver.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from examples.continuous_tamp.primitives import BLOCK_WIDTH, GRASP, sample_region, plan_motion
 from pddlstream.utils import hash_or_id
-from pddlstream.language.constants import partition_facts, NOT, MINIMIZE, get_constraints
+from pddlstream.language.constants import partition_facts, NOT, MINIMIZE, get_constraints, is_parameter
 from pddlstream.language.optimizer import OptimizerOutput
 
 MIN_CLEARANCE = 1e-3 # 0 | 1e-3
@@ -25,7 +25,30 @@ def value_from_var(vars):
         return np.array(new_vars)
     return new_vars
 
+def unbounded_var(model):
+    from gurobipy import GRB
+    return model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY)
+
+def np_var(model, d=2):
+    return np.array([unbounded_var(model) for _ in range(d)])
+
+def copy_model(model):
+    model.update()
+    return model.copy()
+
+##################################################
+
+def collision_constraint(model, name, b1, p1, b2, p2):
+    from gurobipy import GRB
+    dist = unbounded_var(model)
+    abs_dist = unbounded_var(model)
+    model.addConstr(dist, GRB.EQUAL, p2[0] - p1[0], name=name)
+    model.addConstr(BLOCK_WIDTH + MIN_CLEARANCE, GRB.LESS_EQUAL, abs_dist, name=name)
+    model.addGenConstrAbs(abs_dist, dist, name=name)  # abs_
+
 # TODO: partition these
+
+##################################################
 
 def get_optimize_fn(regions, max_time=5, verbose=False):
     # https://www.gurobi.com/documentation/8.1/examples/diagnose_and_cope_with_inf.html
@@ -45,39 +68,25 @@ def get_optimize_fn(regions, max_time=5, verbose=False):
         model.setParam(GRB.Param.OutputFlag, verbose)
         model.setParam(GRB.Param.TimeLimit, max_time)
 
-        def unbounded_var():
-            return model.addVar(lb=-GRB.INFINITY, ub=GRB.INFINITY)
-
-        def np_var(d=2):
-            return np.array([unbounded_var() for _ in range(d)])
-
         var_from_id = {}
         for fact in facts:
             prefix, args = fact[0], fact[1:]
             if prefix == 'conf':
                 param, = args
                 if param not in var_from_id:
-                    var_from_id[hash_or_id(param)] = np_var()
+                    var_from_id[hash_or_id(param)] = np_var(model)
             elif prefix == 'pose':
                 _, param = args
                 if param not in var_from_id:
-                    var_from_id[hash_or_id(param)] = np_var()
+                    var_from_id[hash_or_id(param)] = np_var(model)
             elif prefix == 'traj':
                 raise NotImplementedError()
                 #param, = args
                 #if param not in var_from_id:
-                #    var_from_id[id(param)] = [np_var(), np_var()]
+                #    var_from_id[id(param)] = [np_var(model), np_var(model)]
 
         def get_var(param):
             return var_from_id.get(hash_or_id(param), param)
-
-        def collision_constraint(args, name):
-            b1, p1, b2, p2 = map(get_var, args)
-            dist = unbounded_var()
-            abs_dist = unbounded_var()
-            model.addConstr(dist, GRB.EQUAL, p2[0] - p1[0], name=name)
-            model.addConstr(BLOCK_WIDTH + MIN_CLEARANCE, GRB.LESS_EQUAL, abs_dist, name=name)
-            model.addGenConstrAbs(abs_dist, dist, name=name)  # abs_
 
         #postive, negative, functions = partition_facts(facts)
         objective_terms = []
@@ -100,9 +109,9 @@ def get_optimize_fn(regions, max_time=5, verbose=False):
                 fact = args[0]
                 predicate, args = fact[0], fact[1:]
                 if predicate == 'posecollision':
-                    collision_constraint(args, name)
+                    collision_constraint(model, name, *map(get_var, args))
             elif prefix == 'cfree':
-                collision_constraint(args, name)
+                collision_constraint(model, name, *map(get_var, args))
             elif prefix == 'motion':
                 raise NotImplementedError()
                 #q1, t, q2 = map(get_var, args)
@@ -129,16 +138,99 @@ def get_optimize_fn(regions, max_time=5, verbose=False):
         model.optimize()
         # https://www.gurobi.com/documentation/7.5/refman/optimization_status_codes.html
         if model.status in (GRB.INFEASIBLE, GRB.INF_OR_UNBD): # OPTIMAL | SUBOPTIMAL
-            #diagnose_infeasibility_test(model, get_constraints(facts))
-            #infeasible = identify_infeasibility(facts, model)
-            #infeasible = elastic_filter(model, facts)
-            infeasible = deletion_filter(model, get_constraints(facts))
+            constraint_indices = {i for i, term in enumerate(facts) if term[0] != MINIMIZE}
+            #infeasible = constraint_indices
+            #infeasible = compute_inconsistent(model)
+            #infeasible = deletion_filter(model, constraint_indices)
+            infeasible = elastic_filter(model, constraint_indices)
+            infeasible_facts = [facts[index] for index in sorted(infeasible)]
+            print('Inconsistent:', infeasible_facts)
             return OptimizerOutput(infeasible=[infeasible])
         assignment = tuple(value_from_var(get_var(out)) for out in outputs)
         return OptimizerOutput(assignments=[assignment])
     return fn
 
 ##################################################
+
+def compute_inconsistent(model):
+    # TODO: search over irreducible infeasible sets
+    model.setObjective(0.0)
+    model.computeIIS()
+    #assert model.IISMinimal
+    infeasible = {int(c.constrName) for c in model.getConstrs() if c.IISConstr}
+    return infeasible
+
+def constraints_from_indices(model, indices):
+    names = {str(i) for i in indices}
+    return [c for c in model.getConstrs() if c.constrName in names]
+
+def deletion_filter(original_model, indices):
+    # TODO: apply to general constraint networks
+    from gurobipy import GRB
+    model = original_model.feasibility()
+    prune_constraints = set(model.getConstrs()) - set(constraints_from_indices(model, indices))
+    for c in prune_constraints:
+        model.remove(c)
+    model.optimize()
+    assert model.status == GRB.INFEASIBLE
+
+    inconsistent = set()
+    for index in sorted(indices):
+        temp_model = copy_model(model)
+        constraints = constraints_from_indices(temp_model, {index})
+        if not constraints:
+            continue
+        for c in constraints:
+            temp_model.remove(c)
+        temp_model.optimize()
+        if temp_model.status == GRB.INFEASIBLE:
+            model = temp_model
+        else:
+            inconsistent.add(index)
+    return inconsistent
+
+##################################################
+
+def relax_constraints(model, indices):
+    from gurobipy import GRB
+    # http://www.sce.carleton.ca/faculty/chinneck/docs/ChinneckDravnieks.pdf
+    model.setObjective(0.0)
+    #model = model.feasibility()
+    elastic_constraints = constraints_from_indices(model, indices)
+    objective = model.feasRelax(relaxobjtype=0,
+                                minrelax=True,
+                                vars=[],
+                                lbpen=[],
+                                ubpen=[],
+                                constrs=elastic_constraints,
+                                rhspen=[1.]*len(elastic_constraints))
+    model.optimize()
+    if model.status == GRB.INFEASIBLE:
+        return None
+    #print('Violation:', objective)
+    art_vars = [var for var in model.getVars()
+                if any(var.varname.startswith(p) for p in ['ArtP_', 'ArtN_'])] # Positive & Negative
+    violations = {}
+    for var in art_vars:
+        index = int(var.varname[5:])
+        violations[index] = violations.get(index, 0) + var.x
+    #for index, value in sorted(violations.items(), key=lambda (i, v): v):
+    #    print('{}: {}'.format(facts[index], value))
+    violated = {index for index, value in violations.items() if 1e-6 < value}
+    return violated
+
+def elastic_filter(original_model, indices):
+    elastic = set(indices)
+    inconsistent = set()
+    while True:
+        relaxed_model = copy_model(original_model)
+        violated = relax_constraints(relaxed_model, elastic)
+        if violated is None:
+            break
+        elastic -= violated
+        inconsistent |= violated
+    #return inconsistent
+    return deletion_filter(original_model, inconsistent)
 
 def identify_feasible_subsets(facts, model):
     # TODO: add facts corresponding to feasible subproblems
@@ -162,111 +254,6 @@ def identify_feasible_subsets(facts, model):
     # Use L1 penalization to enforce sparsity. Could even frame as a MILP
     # The assumption is that there is intersection between the failure and a solution
     raise NotImplementedError()
-
-def identify_infeasibility(facts, model):
-    # TODO: search over irreducible infeasible sets
-    model.setObjective(0.0)
-    model.computeIIS()
-    assert model.IISMinimal # Otherwise return all facts
-    #infeasible = set(range(len(facts)))
-    infeasible = {int(c.constrName) for c in model.getConstrs() if c.IISConstr}
-    infeasible_facts = [facts[index] for index in sorted(infeasible)]
-    print('Inconsistent:', infeasible_facts)
-    return infeasible
-
-def constraints_from_indices(model, indices):
-    names = {str(i) for i in indices}
-    return [c for c in model.getConstrs() if c.constrName in names]
-
-def relax_constraints(model, facts, elastic_indices=None):
-    from gurobipy import GRB
-    # http://www.sce.carleton.ca/faculty/chinneck/docs/ChinneckDravnieks.pdf
-    model.setObjective(0.0)
-    #model = model.feasibility()
-    if elastic_indices is None:
-        elastic_indices = set(range(len(facts)))
-    elastic_constraints = constraints_from_indices(model, elastic_indices)
-    #objective = model.feasRelaxS(relaxobjtype=1,  # relax linear constraints
-    #                             minrelax=True,  # Minimum relaxation
-    #                             vrelax=False,  # Variable violations
-    #                             crelax=True) # Constraint violations
-    objective = model.feasRelax(relaxobjtype=0,
-                                minrelax=True,
-                                vars=[],
-                                lbpen=[],
-                                ubpen=[],
-                                constrs=elastic_constraints,
-                                rhspen=[1.]*len(elastic_constraints))
-    model.optimize()
-    if model.status == GRB.INFEASIBLE:
-        return None
-    #print('Violation:', objective)
-    art_vars = [var for var in model.getVars()
-                if any(var.varname.startswith(p) for p in ['ArtP_', 'ArtN_'])] # Positive & Negative
-    violations = {}
-    for var in art_vars:
-        index = int(var.varname[5:])
-        violations[index] = violations.get(index, 0) + var.x
-    #for index, value in sorted(violations.items(), key=lambda (i, v): v):
-    #    print('{}: {}'.format(facts[index], value))
-    violated_indices = {index for index, value in violations.items() if 1e-6 < value}
-    violated_facts = [facts[index] for index in violated_indices]
-    print('Violated:', violated_facts)
-    #assignment = {out: value_from_var(get_var(out)) for out in outputs}
-    #print('Relaxed Assignment:', assignment)
-    return violated_indices
-
-def elastic_filter(original_model, terms):
-    elastic = {i for i, term in enumerate(terms) if term[0] != MINIMIZE}
-    infeasible = set()
-    while True:
-        relaxed_model = copy_model(original_model)
-        violated = relax_constraints(relaxed_model, terms, elastic)
-        if violated is None:
-            break
-        elastic -= violated
-        infeasible |= violated
-    infeasible_facts = [terms[index] for index in infeasible]
-    print('Infeasible:', infeasible_facts)
-    return infeasible
-
-def copy_model(model):
-    model.update()
-    return model.copy()
-
-def deletion_filter(original_model, terms, indices=None):
-    # TODO: apply to general constraint networks
-    from gurobipy import GRB
-    if indices is None:
-        indices = set(range(len(terms)))
-    model = copy_model(original_model)
-    model.setObjective(0.0)
-    #model = model.feasibility()
-    prune_constraints = set(model.getConstrs()) - set(constraints_from_indices(model, indices))
-    for c in prune_constraints:
-        model.remove(c)
-    #model.optimize()
-    #print(model.status, GRB.INFEASIBLE)
-
-    inconsistent = set()
-    for index in sorted(indices):
-        temp_model = copy_model(model)
-        constraints = constraints_from_indices(temp_model, {index})
-        print(constraints)
-        if not constraints:
-            continue
-        for c in constraints:
-            temp_model.remove(c)
-        temp_model.optimize()
-        print(temp_model.status, GRB.INFEASIBLE)
-        if temp_model.status == GRB.INFEASIBLE:
-            model = temp_model
-        else:
-            inconsistent.add(index)
-    inconsistent_facts = [terms[index] for index in inconsistent]
-    print('Infeasible:', inconsistent_facts)
-    raw_input('awef')
-    return inconsistent
 
 ##################################################
 
