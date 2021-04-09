357ea9b67c3141fb43100531c912b067fb320cfd
caelan <caelan@mit.edu>
2021-04-09 01:51:07 -0400

    str_from_obj

--- pddlstream/utils.py
+++ pddlstream/utils.py
@@ -322,24 +322,25 @@ class HeapElement(object):
 
 ##################################################
 
-def sorted_str_from_list(obj):
-    return '[{}]'.format(', '.join(sorted(str_from_object(item) for item in obj)))
+def sorted_str_from_list(obj, **kwargs):
+    return '[{}]'.format(', '.join(sorted(str_from_object(item, **kwargs) for item in obj)))
 
-def str_from_object(obj):  # str_object
+def str_from_object(obj, ndigits=None):  # str_object
     if type(obj) in [list]: #, np.ndarray):
-        return '[{}]'.format(', '.join(str_from_object(item) for item in obj))
+        return '[{}]'.format(', '.join(str_from_object(item, ndigits) for item in obj))
     if type(obj) == tuple:
-        return '({})'.format(', '.join(str_from_object(item) for item in obj))
+        return '({})'.format(', '.join(str_from_object(item, ndigits) for item in obj))
     #if isinstance(obj, dict):
     if type(obj) in [dict, defaultdict, Counter]:
-        return '{{{}}}'.format(', '.join('{}: {}'.format(str_from_object(key), str_from_object(obj[key])) \
-                                  for key in sorted(obj.keys(), key=lambda k: str_from_object(k))))
+        return '{{{}}}'.format(', '.join('{}: {}'.format(str_from_object(key, ndigits), str_from_object(obj[key], ndigits)) \
+                                  for key in sorted(obj.keys(), key=lambda k: str_from_object(k, ndigits))))
     if type(obj) in [set, frozenset]:
-        return '{{{}}}'.format(', '.join(sorted(str_from_object(item) for item in obj)))
-    #if type(obj) in (float, np.float64):
-    #    obj = round(obj, 3)
-    #    if obj == 0: obj = 0  # NOTE - catches -0.0 bug
-    #    return '%.3f' % obj
+        return '{{{}}}'.format(', '.join(sorted(str_from_object(item, ndigits) for item in obj)))
+    if (ndigits is not None) and (type(obj) in [float, np.float64]):
+        obj = round(obj, ndigits=ndigits)
+        if obj == 0.:
+            obj = 0.  # NOTE - catches -0.0 bug
+        return '{0:.{1}f}'.format(obj, ndigits)
     #if isinstance(obj, types.FunctionType):
     #    return obj.__name__
     return str(obj)
