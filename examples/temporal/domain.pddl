ed0bf0b16dda9709afa704c031c285382a0a6b68
caelan <caelan@mit.edu>
2021-06-17 14:08:04 -0400

    Refactor

---
+++ examples/temporal/domain.pddl
@@ -0,0 +1,80 @@
+(define (domain temporal)
+  (:predicates
+    (Time ?t)
+    (Duration ?dt)
+    (Sum ?t1 ?dt ?t2)
+    (GE ?t1 ?t2)
+
+    (Food ?f)
+    (Stove ?s) ; TODO: Oven
+
+    (CookDuration ?dt ?f ?s)
+    (Cooking ?t ?f ?s)
+
+    (AtTime ?t)
+    (Cooked ?f)
+    (Locked ?s)
+    (Premature ?t)
+    (Invalid)
+  )
+  (:functions
+    (Elapsed ?dt)
+  )
+  (:action wait
+    :parameters (?t1 ?dt ?t2)
+    :precondition (and (Sum ?t1 ?dt ?t2)
+                       (AtTime ?t1)
+                       ;(CanWait)
+                       (not (Premature ?t2))
+                       (not (Invalid))
+                  )
+    :effect (and (AtTime ?t2)
+                 (not (AtTime ?t1))
+                 (increase (total-cost) (Elapsed ?dt)))
+  )
+  (:action start-cooking
+    :parameters (?t1 ?dt ?t2 ?f ?s)
+    :precondition (and ;(Food ?f) (Stove ?s)
+                       (CookDuration ?dt ?f ?s)
+                       (Sum ?t1 ?dt ?t2)
+                       (AtTime ?t1)
+                       (not (Locked ?f)) (not (Locked ?s))
+                       ;(not (Premature ?t1))
+                       (not (Invalid))
+                   )
+    :effect (and (Cooking ?t2 ?f ?s)
+                 (Locked ?f) (Locked ?s)
+                 (increase (total-cost) 0))
+  )
+  ; TODO: while cooking
+  (:action stop-cooking
+    :parameters (?t2 ?f ?s)
+    :precondition (and (Time ?t2) (Food ?f) (Stove ?s)
+                       (Cooking ?t2 ?f ?s)
+                       (AtTime ?t2)
+                       ;(not (Premature ?t2))
+                       (not (Invalid))
+                  )
+    :effect (and (Cooked ?f)
+                 (not (Cooking ?t2 ?f ?s))
+                 (not (Locked ?f)) (not (Locked ?s))
+                 (increase (total-cost) 0))
+  )
+
+  ;# TODO: overall conditions
+  ;(:derived (Valid) (and
+  ;  (forall (?t ?f ?s) (imply (Cooking ?t ?f ?s)
+  ;                            (and ...)))
+  ;))
+
+  ;(:derived (Invalid) (or
+  ;  (exists (?t ?f ?s) (and (Cooking ?t ?f ?s)
+  ;                           ...))
+  ;))
+
+  (:derived (Premature ?t2) (or
+    (exists (?t1 ?f ?s) (and (Food ?f) (Stove ?s)
+                             (GE ?t2 ?t1) (not (= ?t2 ?t1)) ; TODO: strictly greater than?
+                             (Cooking ?t1 ?f ?s)))
+  ))
+)
--- examples/temporal/run.py
+++ examples/temporal/run.py
@@ -6,92 +6,11 @@ from itertools import product
 
 from pddlstream.algorithms.meta import solve, create_parser
 from pddlstream.language.constants import PDDLProblem, Or, Exists, print_solution, Output, And
+from pddlstream.utils import read_pddl
 from pddlstream.language.stream import StreamInfo
 from pddlstream.language.generator import from_fn, from_test
 
 # TODO: 2 modes - check for possible inconsistency before or fail if inconsistency is detected after
-DOMAIN_PDDL = """
-(define (domain temporal)
-  (:predicates 
-    (Time ?t)
-    (Duration ?dt)
-    (Sum ?t1 ?dt ?t2)
-    (GE ?t1 ?t2)
-    
-    (Food ?f)
-    (Stove ?s) ; TODO: Oven
-    
-    (CookDuration ?dt ?f ?s)
-    (Cooking ?t ?f ?s)
-
-    (AtTime ?t)
-    (Cooked ?f)
-    (Locked ?s)
-    (Premature ?t)
-    (Invalid)
-  )
-  (:functions
-    (Elapsed ?dt)
-  )
-  (:action wait
-    :parameters (?t1 ?dt ?t2)
-    :precondition (and (Sum ?t1 ?dt ?t2) 
-                       (AtTime ?t1)
-                       ;(CanWait)
-                       (not (Premature ?t2))
-                       (not (Invalid))
-                  )
-    :effect (and (AtTime ?t2) 
-                 (not (AtTime ?t1))
-                 (increase (total-cost) (Elapsed ?dt)))
-  )
-  (:action start-cooking
-    :parameters (?t1 ?dt ?t2 ?f ?s)
-    :precondition (and ;(Food ?f) (Stove ?s)
-                       (CookDuration ?dt ?f ?s)
-                       (Sum ?t1 ?dt ?t2)
-                       (AtTime ?t1)
-                       (not (Locked ?f)) (not (Locked ?s))
-                       ;(not (Premature ?t1))
-                       (not (Invalid))
-                   )
-    :effect (and (Cooking ?t2 ?f ?s)
-                 (Locked ?f) (Locked ?s)
-                 (increase (total-cost) 0))
-  )
-  ; TODO: while cooking
-  (:action stop-cooking
-    :parameters (?t2 ?f ?s)
-    :precondition (and (Time ?t2) (Food ?f) (Stove ?s)
-                       (Cooking ?t2 ?f ?s)
-                       (AtTime ?t2)
-                       ;(not (Premature ?t2))
-                       (not (Invalid))
-                  )
-    :effect (and (Cooked ?f)
-                 (not (Cooking ?t2 ?f ?s))
-                 (not (Locked ?f)) (not (Locked ?s))
-                 (increase (total-cost) 0))
-  )
-  
-  ;# TODO: overall conditions
-  ;(:derived (Valid) (and
-  ;  (forall (?t ?f ?s) (imply (Cooking ?t ?f ?s)
-  ;                            (and ...)))
-  ;))
-  
-  ;(:derived (Invalid) (or
-  ;  (exists (?t ?f ?s) (and (Cooking ?t ?f ?s) 
-  ;                           ...))
-  ;))
-    
-  (:derived (Premature ?t2) (or 
-    (exists (?t1 ?f ?s) (and (Food ?f) (Stove ?s)
-                             (GE ?t2 ?t1) (not (= ?t2 ?t1)) ; TODO: strictly greater than?
-                             (Cooking ?t1 ?f ?s)))
-  ))
-)
-"""
 
 # TODO: see counting, satisfy, cashpoint, discrete_belief, ...
 STREAM_PDDL = """
@@ -153,7 +72,10 @@ def create_problem(max_time=20., n_foods=3, n_stoves=2):
         goal_expressions.append(('Cooked', food))
     goal = And(*goal_expressions)
 
-    return PDDLProblem(DOMAIN_PDDL, constant_map, STREAM_PDDL, stream_map, init, goal)
+    domain_pddl = read_pddl(__file__, 'domain.pddl')
+    stream_pddl = STREAM_PDDL
+
+    return PDDLProblem(domain_pddl, constant_map, stream_pddl, stream_map, init, goal)
 
 ##################################################
 
@@ -173,8 +95,9 @@ def main():
         'ge': StreamInfo(eager=True, verbose=False),
     }
 
+    # TODO: eager for incremental
     solution = solve(problem, algorithm=args.algorithm, unit_costs=args.unit,
-                     stream_info=info, planner='ff-wastar1',
+                     stream_info=info, planner='max-astar',
                      effort_weight=None, debug=True, verbose=False)
     print_solution(solution)
 
